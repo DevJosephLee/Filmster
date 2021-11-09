2520db0a9834802cecb624247a2137f213ba6135
Joseph Lee <teddylou0215@gmail.com>
2021-11-09 21:10:36 +0000

    Create a ul element that will be used to append search results.#4

--- js/data.js
+++ js/data.js
@@ -3,3 +3,13 @@ data = {
   view: 'search-form',
   searched: ''
 }
+
+var previousDataJSON = localStorage.getItem('movie-finder-local-storage');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+
+window.addEventListener('beforeunload', function (event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('movie-finder-local-storage', dataJSON);
+})
