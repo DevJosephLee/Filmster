ad9b9f41586c4e960bfbf1f770ea32fa50ee5c83
Joseph Lee <teddylou0215@gmail.com>
2021-11-10 04:28:26 +0000

    Create a function that will generate a DOM Tree that looks like the dummy entry.#6

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var $searchInput = document.querySelector('.search-input');
 var $logoButton = document.querySelector('.logo-button');
 var $backButton = document.querySelector('.back-button');
 var $viewNodeList = document.querySelectorAll('.view');
+var $searchedList = document.querySelector('.searched-list');
 
 
 function search() {
@@ -11,8 +12,50 @@ function search() {
   xhr.open('GET', 'https://api.themoviedb.org/3/search/movie?api_key=9ed2615a579d77bb72ade801a8902712&query=' + $searchInput.value);
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
-    console.log(xhr.status);
-    console.log(xhr.response);
+    for (var i = 0; i < xhr.response.results.length; i++) {
+      var title = xhr.response.results[i].title;
+      var releaseYear = xhr.response.results[i].release_date.slice(0, 4);
+      var posterPath = 'https://image.tmdb.org/t/p/w500' + xhr.response.results[i].poster_path;
+      var overview = xhr.response.results[i].overview;
+      function generateSearchedMoviesResults() {
+        var $root = document.createElement('li');
+
+        var $searchResultCardDiv = document.createElement('div');
+        $searchResultCardDiv.className = 'search-result-card border-bottom-white padding-top';
+        $root.appendChild($searchResultCardDiv);
+
+        var $textAlignCenterDiv = document.createElement('div');
+        $textAlignCenterDiv.className = 'text-align-center';
+        $searchResultCardDiv.appendChild($textAlignCenterDiv);
+
+        var $posterImage = document.createElement('img');
+        $posterImage.setAttribute('src', posterPath);
+        $posterImage.className = 'search-result-poster';
+        $textAlignCenterDiv.appendChild($posterImage);
+
+        var $searchResultInfoDiv = document.createElement('div');
+        $searchResultInfoDiv.className = 'search-result-info';
+        $searchResultCardDiv.appendChild($searchResultInfoDiv);
+
+        var $searchResultName = document.createElement('h3');
+        $searchResultName.className = 'search-result-name';
+        $searchResultName.textContent = title + ' ' + '(' + releaseYear + ')';
+        $searchResultInfoDiv.appendChild($searchResultName);
+
+        var $searchResultOverview = document.createElement('p');
+        $searchResultOverview.className = 'search-result-overview';
+        $searchResultOverview.textContent = overview;
+        $searchResultInfoDiv.appendChild($searchResultOverview);
+
+        var $infoButton = document.createElement('button');
+        $infoButton.className = 'info-button';
+        $infoButton.textContent = 'INFO';
+        $searchResultInfoDiv.appendChild($infoButton);
+
+        return $root;
+      }
+      $searchedList.append(generateSearchedMoviesResults());
+    }
   });
   xhr.send();
 }
@@ -43,3 +86,13 @@ function backButton() {
 }
 
 $backButton.addEventListener('click', backButton);
+
+// function switchViews(event) {
+//   for (var i = 0; i < $viewNodeList.length; i++) {
+//     if (event.target.getAttribute('data-view') !== $viewNodeList[i].getAttribute('data-view')) {
+//       $viewNodeList[i].className = 'view hidden';
+//     } else {
+//       $viewNodeList[i].className = 'view';
+//     }
+//   }
+// }
